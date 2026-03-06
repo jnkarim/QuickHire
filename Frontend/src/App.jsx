e67be082b86ab69a3b4049f2830b3863f4e39e72
jnkarim <julkernkarim@gmail.com>
2026-03-06 16:21:57 +0600

    change in files for admin pannel

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
 import JobsPage from "./pages/JobsPage";
 import JobDetailPage from "./pages/JobDetailPage";
+import AdminPage from "./pages/AdminPage";
 
 
 function ScrollToTop() {
@@ -20,6 +21,8 @@ export default function App() {
             <Route path="/" element={<HomePage />} />
             <Route path="/jobs" element={<JobsPage />} />
             <Route path="/jobs/:id" element={<JobDetailPage />} />
+            <Route path="/admin" element={<AdminPage />} />
+            <Route path="/admin/new" element={<AdminPage />} />
             <Route
               path="*"
               element={
